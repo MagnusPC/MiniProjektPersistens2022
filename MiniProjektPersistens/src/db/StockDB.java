e12a803f19c212c1067f88b81157915713b087db
oskarbucn <10434661@ucn.dk>
2022-10-13 13:49:19 +0200

    StockDB WIP

--- MiniProjektPersistens/src/db/StockDB.java
+++ MiniProjektPersistens/src/db/StockDB.java
@@ -1,6 +1,17 @@
 package db;
 
+import model.*;
+
 public class StockDB implements StockDBIF{
 
 	
+	
+	public void updateStock(Order order) {
+		for (int i = 0; i < order.getOrderLines().length(); i++) {
+			ol = order.getOrderLines().get(i);
+			q = ol.getQuantity();
+			pId = ol.getProductId();
+			
+		}
+	}
 }
