9db2a18acc0b78f653ee3471a6bfbf807123d312
MagnusPC <10434658@ucn.dk>
2022-10-13 13:20:40 +0200

    prod

--- MiniProjektPersistens/src/model/GunReplica.java
+++ MiniProjektPersistens/src/model/GunReplica.java
@@ -1,5 +1,16 @@
 package model;
 
-public class GunReplica {
+public class GunReplica extends Product {
+	
+	private int id;
+	private String caliber;
+	private String material;
+	
+	public GunReplica(int id, String caliber, String material, String name, double purchasePrice, double salePrice, double rentPrice, String productType, int supplierID) {
+		super(name, purchasePrice, salePrice, rentPrice, productType, supplierID);
+		this.id = id;
+		this.caliber = caliber;
+		this.material = material;
+	}
 
 }
--- MiniProjektPersistens/src/model/Product.java
+++ MiniProjektPersistens/src/model/Product.java
@@ -1,5 +1,106 @@
 package model;
 
-public class Product {
+public abstract class Product {
+	
+	private String name;
+	private double purchasePrice;
+	private double salePrice;
+	private double rentPrice;
+	private String productType;
+	private int supplierID;
 
+	public Product(String name, double purchasePrice, double salePrice, double rentPrice, String productType, int supplierID) {
+		//super(); til datetime
+		this.name = name;
+		this.purchasePrice = purchasePrice;
+		this.salePrice = salePrice;
+		this.rentPrice = rentPrice;
+		this.productType = productType;
+		this.supplierID = supplierID;
+	}
+	
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the purchasePrice
+	 */
+	public double getPurchasePrice() {
+		return purchasePrice;
+	}
+
+	/**
+	 * @param purchasePrice the purchasePrice to set
+	 */
+	public void setPurchasePrice(double purchasePrice) {
+		this.purchasePrice = purchasePrice;
+	}
+
+	/**
+	 * @return the salePrice
+	 */
+	public double getSalePrice() {
+		return salePrice;
+	}
+
+	/**
+	 * @param salePrice the salePrice to set
+	 */
+	public void setSalePrice(double salePrice) {
+		this.salePrice = salePrice;
+	}
+
+	/**
+	 * @return the rentPrice
+	 */
+	public double getRentPrice() {
+		return rentPrice;
+	}
+
+	/**
+	 * @param rentPrice the rentPrice to set
+	 */
+	public void setRentPrice(double rentPrice) {
+		this.rentPrice = rentPrice;
+	}
+
+	/**
+	 * @return the productType
+	 */
+	public String getProductType() {
+		return productType;
+	}
+
+	/**
+	 * @param productType the productType to set
+	 */
+	public void setProductType(String productType) {
+		this.productType = productType;
+	}
+
+	/**
+	 * @return the supplierID
+	 */
+	public int getSupplierID() {
+		return supplierID;
+	}
+
+	/**
+	 * @param supplierID the supplierID to set
+	 */
+	public void setSupplierID(int supplierID) {
+		this.supplierID = supplierID;
+	}
+	
 }
