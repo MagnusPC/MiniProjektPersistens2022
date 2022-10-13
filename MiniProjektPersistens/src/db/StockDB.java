7c535524e6e4c15f695a2594c3ca995ae0287a4d
oskarbucn <10434661@ucn.dk>
2022-10-13 14:07:04 +0200

    Update StockDB.java

--- MiniProjektPersistens/src/db/StockDB.java
+++ MiniProjektPersistens/src/db/StockDB.java
@@ -7,11 +7,13 @@ public class StockDB implements StockDBIF{
 	
 	
 	public void updateStock(Order order) {
+		String sqlQuery = "";
 		for (int i = 0; i < order.getOrderLines().length(); i++) {
-			ol = order.getOrderLines().get(i);
-			q = ol.getQuantity();
-			pId = ol.getProductId();
+			OrderLine ol = order.getOrderLines().get(i);
+			int q = ol.getQuantity();
+			int pId = ol.getProductId();
 			
+			String sqlQuery2 = ("select stockId from Product where serialNumber = " + pId);
 		}
 	}
 }
