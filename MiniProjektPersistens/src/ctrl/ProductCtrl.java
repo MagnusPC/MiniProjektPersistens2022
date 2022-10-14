9e0031a8794b7d4f8db4b70aab7fbf154c35395c
oskarbucn <10434661@ucn.dk>
2022-10-14 09:14:02 +0200

    Update ProductCtrl.java

--- MiniProjektPersistens/src/ctrl/ProductCtrl.java
+++ MiniProjektPersistens/src/ctrl/ProductCtrl.java
@@ -1,5 +1,28 @@
 package ctrl;
 
+import db.*;
+import model.*;
+
 public class ProductCtrl {
 
+	private ProductDB productDB;
+	private StockDB stockDB;
+	
+	
+	
+	public Product findProductByID(int productId) {
+		Product result = null;
+		try {
+			result = productDB.findProductByID(productId);
+		} catch (DataAccessException e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+	
+	
+	public void updateStock(Order order) {
+		stockDB.updateStock(order);
+	}
+	
 }
