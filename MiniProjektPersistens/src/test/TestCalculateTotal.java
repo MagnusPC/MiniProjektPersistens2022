04332a1848ea9d9eab06f299c5cbfa1f9acc9b4b
MagnusPC <10434658@ucn.dk>
2022-10-16 13:23:35 +0200

    Update TestCalculateTotal.java

--- MiniProjektPersistens/src/test/TestCalculateTotal.java
+++ MiniProjektPersistens/src/test/TestCalculateTotal.java
@@ -1,12 +1,12 @@
 package test;
 
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
 import java.sql.SQLException;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import ctrl.OrderCtrl;
@@ -15,39 +15,39 @@ import model.Order;
 
 class TestCalculateTotal {
     
-    private OrderCtrl oCtrl1;
+    private OrderCtrl oCtrl;
 
 	@BeforeEach
 	void setUp() throws Exception {
-	    oCtrl1 = new OrderCtrl();
+	    oCtrl = new OrderCtrl();
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
-	    oCtrl1 = null;
+	    oCtrl = null;
 	}
 
 	@Test
-	void testDeliveryPriceAdded() throws DataAccessException, SQLException {
+	void testDeliveryPriceAddedValidOrder() throws DataAccessException, SQLException {
 		//ARRANGE
         //We make the orders
-	    oCtrl1.createNewOrder();
+	    oCtrl.createNewOrder();
 	    
 	    //ACT
         //We add objects to the orders
-	    oCtrl1.addCustomerByPhoneNo("+45 97971010");
-	    oCtrl1.addProductByProductId(1, 2);
-	    oCtrl1.addProductByProductId(2, 1);
-	    oCtrl1.addProductByProductId(3, 1);
+	    oCtrl.addCustomerByPhoneNo("+45 97971010");
+	    oCtrl.addProductByProductId(1, 2);
+	    oCtrl.addProductByProductId(2, 1);
+	    oCtrl.addProductByProductId(3, 1);
 	    
 	    //We save the expected total of Order 1 for later
 	    double tempTotal = (299.99*2)+119.19+165.29;
 	    
 	    //We add the invoice, and thus the total, to the orders
-	    oCtrl1.addInvoice();
+	    oCtrl.addInvoice();
 	  
 	    //We make the total retrieveable
-	    Order tempO = oCtrl1.finishOrder();
+	    Order tempO = oCtrl.finishOrder();
 	    
 	    //ASSERT
 	    //We check to see that the delivery price has been added
@@ -55,33 +55,43 @@ class TestCalculateTotal {
 	}
 	
 	@Test
-	void testDeliveryPriceNotAdded() throws DataAccessException, SQLException {
+	void testDeliveryPriceNotAddedValidOrder() throws DataAccessException, SQLException {
 	    //arrange
-	    oCtrl1.createNewOrder();
+	    oCtrl.createNewOrder();
 	    //act
-        oCtrl1.addCustomerByPhoneNo("+45 97971010");
-        oCtrl1.addProductByProductId(1, 9);
-        oCtrl1.addInvoice();
-        Order tempO = oCtrl1.finishOrder();
+        oCtrl.addCustomerByPhoneNo("+45 97971010");
+        oCtrl.addProductByProductId(1, 9);
+        oCtrl.addInvoice();
+        Order tempO = oCtrl.finishOrder();
         //assert
         assertEquals(299.99*9, tempO.getInvoice().getInvoiceAmount(), 0.0001);
 	}
 	
+	@Disabled("Better to make finishOrder fail")
 	@Test
-	void testDiscountAdded() throws DataAccessException, SQLException {
+	void testDeliveryPriceNotAddedInvalidOrder() throws DataAccessException {
+	    oCtrl.createNewOrder();
+	    oCtrl.addCustomerByPhoneNo("+45 11111111");
+	    oCtrl.addInvoice();
+	    Order tempO = oCtrl.finishOrder();
+	    assertEquals(0, tempO.getInvoice().getInvoiceAmount());
+	}
+	
+	@Test
+	void testDiscountAddedValidOrder() throws DataAccessException, SQLException {
 	    //ARRANGE
-        oCtrl1.createNewOrder();
+        oCtrl.createNewOrder();
         
         //ACT
         //We add objects to the orders
-        oCtrl1.addCustomerByPhoneNo("+45 88330000");
-        oCtrl1.addProductByProductId(1, 4);
+        oCtrl.addCustomerByPhoneNo("+45 88330000");
+        oCtrl.addProductByProductId(1, 4);
         
         //We add the invoice, and thus the total, to the orders
-        oCtrl1.addInvoice();
+        oCtrl.addInvoice();
         
         //We make the total retrieveable
-        Order tempO = oCtrl1.finishOrder();
+        Order tempO = oCtrl.finishOrder();
         
         //ASSERT
         //We check to see that the discount has not been added
@@ -89,14 +99,14 @@ class TestCalculateTotal {
 	}
 	
 	@Test
-	void testDiscountNotAdded() throws DataAccessException, SQLException {
+	void testDiscountNotAddedValidOrder() throws DataAccessException, SQLException {
 	    //arrange
-	    oCtrl1.createNewOrder();
+	    oCtrl.createNewOrder();
 	    //act 
-        oCtrl1.addCustomerByPhoneNo("+45 88330000");
-        oCtrl1.addProductByProductId(1, 6);
-        oCtrl1.addInvoice();
-        Order tempO = oCtrl1.finishOrder();
+        oCtrl.addCustomerByPhoneNo("+45 88330000");
+        oCtrl.addProductByProductId(1, 6);
+        oCtrl.addInvoice();
+        Order tempO = oCtrl.finishOrder();
         //assert
         assertEquals((299.99*6)*0.85, tempO.getInvoice().getInvoiceAmount(), 0.0001);
 	}
